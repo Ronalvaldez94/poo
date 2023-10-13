0295044bd07fa574ea5d10979aab6c27261f44d8
Ronalvaldez94 <144462082+Ronalvaldez94@users.noreply.github.com>
2023-10-13 13:57:26 -0500

    Update Automovil.java

--- src/pr1/Automovil.java
+++ src/pr1/Automovil.java
@@ -1,8 +1,8 @@
-
 package pr1;
 
 public class Automovil extends Vehiculo {
-    private int numeroPuertas; 
+
+    private int numeroPuertas;
 
     public Automovil(int numeroPuertas, String marca, String modelo, int año, String propietario, String placa) {
         super(marca, modelo, año, propietario, placa);
@@ -16,17 +16,16 @@ public class Automovil extends Vehiculo {
     public void setNumeroPuertas(int numeroPuertas) {
         this.numeroPuertas = numeroPuertas;
     }
-   
-    
-    @Override
-    public void mostrarInformacion(){
-        System.out.println("El numero de puertas del vehiculo es: " +numeroPuertas);
-        System.out.println("La marca del vehiculo es: " +marca);
-        System.out.println("El modelo del vehiculo es: " +modelo);
-        System.out.println("El año de fabricacion del vehiculo es: " +año); 
-        System.out.println("El propietario del vehiculo es: " +propietario);
-        System.out.println("La placa del vehiculo es: " +placa);
 
-   }
-     
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("El numero de puertas del vehiculo es: " + numeroPuertas);
+        System.out.println("La marca del vehiculo es: " + marca);
+        System.out.println("El modelo del vehiculo es: " + modelo);
+        System.out.println("El anio de fabricacion del vehiculo es: " + año);
+        System.out.println("El propietario del vehiculo es: " + propietario);
+        System.out.println("La placa del vehiculo es: " + placa);
+
+    }
 }
+
