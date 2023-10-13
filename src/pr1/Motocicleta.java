5658a199dad90ef552fff44b661fed6362f5af45
Ronalvaldez94 <144462082+Ronalvaldez94@users.noreply.github.com>
2023-10-13 13:56:51 -0500

    Update Motocicleta.java

--- src/pr1/Motocicleta.java
+++ src/pr1/Motocicleta.java
@@ -1,9 +1,9 @@
 
 package pr1;
 
-
 public class Motocicleta extends Vehiculo {
-   private double cilindraje;  
+
+    private double cilindraje;
 
     public Motocicleta(double cilindraje, String marca, String modelo, int año, String propietario, String Placa) {
         super(marca, modelo, año, propietario, Placa);
@@ -17,17 +17,15 @@ public class Motocicleta extends Vehiculo {
     public void setCilindraje(double cilindraje) {
         this.cilindraje = cilindraje;
     }
-   
- 
-   @Override
-   public void mostrarInformacion(){
-        System.out.println("El Cilindraje de la motocicleta es: " +cilindraje);
-        System.out.println("La marca de la motocicleta es: " +marca);
-        System.out.println("El modelo de la motocicleta es: " +modelo);
-        System.out.println("El año de fabricacion de la motocicleta es: " +año); 
-        System.out.println("El propietario de la motocicleta es: " +propietario);
-        System.out.println("La placa de la motocicleta es: " +placa);
 
-   }
-    
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("El Cilindraje de la motocicleta es: " + cilindraje);
+        System.out.println("La marca de la motocicleta es: " + marca);
+        System.out.println("El modelo de la motocicleta es: " + modelo);
+        System.out.println("El anio de fabricacion de la motocicleta es: " + año);
+        System.out.println("El propietario de la motocicleta es: " + propietario);
+        System.out.println("La placa de la motocicleta es: " + placa);
+
+    }
 }
